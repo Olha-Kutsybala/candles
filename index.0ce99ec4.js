b4046e9d2c7836a525c617eed30fed832c57bb82
Olha-Kutsybala <116167322+Olha-Kutsybala@users.noreply.github.com>
2023-08-10 09:34:30 +0000

    Deploying to gh-pages from @ Olha-Kutsybala/candles@b811dcc93c0e801890ddfdb1e482873193a5fcdd 🚀

---
+++ index.0ce99ec4.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".menu__btn"),n=document.querySelector(".header__menu-list");e.addEventListener("click",(function(){n.classList.toggle("menu-open")}))}();
+//# sourceMappingURL=index.0ce99ec4.js.map
---
+++ index.d54e678a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".menu__btn"),t=document.querySelector(".header__menu-list");e.addEventListener("click",(()=>{t.classList.toggle("menu-open")}));
+//# sourceMappingURL=index.d54e678a.js.map
